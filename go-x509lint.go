c43ca54e5d3dbea6f0466a9c544387627b201267
Rob Stradling <rob@comodoca.com>
2018-09-03 16:52:36 +0100

    Copyright boilerplate; return a single multi-line string; remove test code.

--- go-x509lint.go
+++ go-x509lint.go
@@ -1,7 +1,24 @@
+/* go-x509lint - Go wrapper for github.com/kroeckx/x509lint
+ * Written by Rob Stradling
+ * Copyright (C) 2018 COMODO CA Limited
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package x509lint
 
 import (
-	"strings"
 	"unsafe"
 )
 
@@ -18,30 +35,11 @@ func Init() {
 	C.check_init()
 }
 
-func Check(cert_der []byte, cert_type int) []string {
+func Check(cert_der []byte, cert_type int) string {
 	C.check((*C.uchar)(unsafe.Pointer(&cert_der[0])), (C.ulong)(len(cert_der)), C.DER, (C.CertType)(cert_type))
-	return strings.Split(C.GoString(C.get_messages()), "\n")
+	return C.GoString(C.get_messages())
 }
 
 func Finish() {
 	C.check_finish()
 }
-
-/*func main() {
-	C.check_init()
-
-	cert, err := ioutil.ReadFile("example.cer")
-	if err != nil {
-		fmt.Printf("ERROR: %v", err)
-	}
-
-	cert_type := C.SubscriberCertificate
-
-	C.check((*C.uchar)(unsafe.Pointer(&cert[0])), (C.ulong)(len(cert)), C.DER, (C.CertType)(cert_type))
-
-	messages := C.GoString(C.get_messages())
-	fmt.Printf(messages)
-
-	C.check_finish()
-}
-*/
